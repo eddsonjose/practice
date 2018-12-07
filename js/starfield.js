4aa5e9d0a9424d50c16747e438cafc615ebf9090
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2018-12-07 12:16:30 -0800

    Update starfield.js

--- js/starfield.js
+++ js/starfield.js
@@ -8,7 +8,7 @@ var yy = 0;
 var easing = 0.05;
 
   function setup() {
-    createCanvas(width, height, WEBGL);
+    createCanvas(width, height);
     star = new Star();
       
     for (var i = 0; i < 1600; i++) {
