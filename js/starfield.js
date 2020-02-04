1e9590631b13d1ee76c79ddd592bb111a7933791
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2020-02-03 23:35:24 -0800

    update

--- js/starfield.js
+++ js/starfield.js
@@ -1,4 +1,4 @@
-//code edited from Chris Nielsen: 
+//code edited from Chris Nielsen:
 //http://bluegalaxy.info/codewalk/2018/01/04/p5-js-build-starfield/
 
 var stars = [];
@@ -9,16 +9,18 @@ var easing = 0.05;
 
 
   function setup() {
-    createCanvas(775, 440, WEBGL);
+    frameRate(10);
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    // createCanvas(775, 440, WEBGL);
     star = new Star();
-      
+
     for (var i = 0; i < 1600; i++) {
       stars[i] = new Star();
     }
   }
 
   function draw() {
-    speed = map(5, 0, width, 10, 30);
+    speed = map(5, 0, width, 30, 30);
     background(10, 0, 50);
     translate(-width/2, -height/2);
 		var targetX = mouseX;
@@ -29,18 +31,18 @@ var easing = 0.05;
     var dy = targetY - yy;
     yy += dy * easing;
     translate(xx, yy, -200)
-    
+
     for (var i = 0; i < stars.length; i++) {
       stars[i].update();
       stars[i].show();
-    } 
+    }
   }
 
 class Star {
   constructor() {
     this.x = random(-width, width);
     this.y = random(-height, height);
-    this.z = random(width);      
+    this.z = random(width);
   }
 
   update() {
@@ -52,20 +54,15 @@ class Star {
 
     }
   }
- 
+
   show() {
-    fill(random(0, 255), random(0, 255), random(0, 255));
-		noStroke();          
+    fill(random(150, 255), random(150, 255), random(150, 255));
+		// noStroke();
+    stroke(random(150, 255), random(150, 255), random(150, 255));
+    strokeWeight(random(1, 10));
     var sx = map(this.x/this.z, 0, 1, 0, width);
     var sy = map(this.y/this.z, 0, 1, 0, height);
     var r = map(this.z, 0, width, 12, 0);
-    ellipse(sx, sy, r, r);    
+    ellipse(sx, sy, r, r);
   }
 }
-
-
-
-
-
-
-
